b702867d473e996b28213370000184518d26d55d
Brendan Obilo <brendanxure@outlook.com>
2024-01-21 20:18:15 +0100

    First Commit

---
+++ python_practice.py
@@ -0,0 +1,15 @@
+########################################################################
+# Name:         Brendan Obilo
+# Date:         Jan 12th, 2024
+# Program:      Practice Python
+# Description:  Learning most basics of python
+#
+# 
+#
+#######################################################################
+
+years_to_experience = 3
+
+print("Name: \t\t\tBrendan Obilo") 
+print("Program: \t\tPython")
+print("Years to complete: \t" + str(years_to_experience))
