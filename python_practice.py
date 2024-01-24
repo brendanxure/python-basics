6e2c02984113ca20bd202adcb4a8c34a8c934231
Brendan Obilo <brendanxure@outlook.com>
2024-01-24 17:07:57 +0100

    update

--- python_practice.py
+++ python_practice.py
@@ -15,3 +15,34 @@ print("Program: \t\tPython")
 print("Years to complete: \t" + str(years_to_experience))
 print(type([1, 2, 3, 5]))
 print(type({"Name:", "Brendan"}))
+
+# Initialization of variables
+slices_of_pizza = 50
+each_programmer_eats = 3
+addition = 0
+subraction = 0
+multiplication = 0
+division = 0.0
+pizza_remainder = 0
+
+# Addition
+addition = 120 + 43
+
+# Subraction
+subraction = 9382 - 450
+
+# Multiplication
+multiplication = 999 * 999
+
+# Division
+division = 120 / 40
+
+# Modulo
+pizza_remainder = slices_of_pizza % each_programmer_eats
+
+# output
+print(addition)
+print(subraction)
+print(multiplication)
+print(division)
+print(pizza_remainder)
