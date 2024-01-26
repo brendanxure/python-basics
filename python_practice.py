7a81b41a8d92ad2a19caee20a4de078acd93ed1c
Brendan Obilo <brendanxure@outlook.com>
2024-01-26 20:46:15 +0100

    update

--- python_practice.py
+++ python_practice.py
@@ -7,62 +7,51 @@
 # 
 #
 #######################################################################
-
-years_to_experience = 3
-
-print("Name: \t\t\tBrendan Obilo") 
-print("Program: \t\tPython")
-print("Years to complete: \t" + str(years_to_experience))
-print(type([1, 2, 3, 5]))
-print(type({"Name:", "Brendan"}))
-
-# Initialization of variables
-slices_of_pizza = 50
-each_programmer_eats = 3
-addition = 0
-subraction = 0
-multiplication = 0
-division = 0.0
-pizza_remainder = 0
-
-# Addition
-addition = 120 + 43
-
-# Subraction
-subraction = 9382 - 450
-
-# Multiplication
-multiplication = 999 * 999
-
-# Division
-division = 120 / 40
-
-# Modulo
-pizza_remainder = slices_of_pizza % each_programmer_eats
-
-# output
-print(addition)
-print(subraction)
-print(multiplication)
-print(division)
-print(pizza_remainder)
+# import math library
+import math
 
 # Initialize variables
-first_number = 0.0
-second_number = 0.0
-average_number = 0.0
 
-# Declaration of constant
-AVERAGE = 2
+# length_of_reservoir = 0.0
+length_of_reservoir = 0.0
 
-# Request user to enter first number
-first_number = float(input("Enter your first number "))
+# width_of_reservoir = 0.0
+width_of_reservoir = 0.0
 
-# Request user to enter second number
-second_number = float(input("Enter your second number "))
+# diameter_of_duck = 0.0
+diameter_of_duck = 0.0
 
-# Get the average of the two numbers
-average_number = float((first_number + second_number)/AVERAGE)
+# total_rubber_ducks = 0
+total_rubber_ducks = 0
 
-# Print the required outcome
-print(average_number)
+# Declare constant
+# MINIMUM_DIAMETER = 0
+MINIMUM_DIAMETER = 0
+
+# collect data from user
+
+# print(Please enter the length of reservoir in cm)
+# length_of_reservoir = user_input()
+length_of_reservoir = float(input("Enter the length of reservoir in cm "))
+
+# print(Please enter the width of reservoir in cm)
+# width_of_reservoir = user_input()
+width_of_reservoir = float(input("Enter the width of reservoir in cm "))
+
+# print(Please enter the diameter of reservoir in cm)
+# diameter_of_duck = user_input()
+diameter_of_duck = float(input("Enter the diameter of reservoir in cm "))
+
+# if user inputs diameter of 0
+if diameter_of_duck <= MINIMUM_DIAMETER:
+    print("Diameter should be greater than 0")
+else:
+# calculate the total number of rubber ducks
+# total_rubber_ducks = [length_of_reservoir/diameter_of_duck] 
+#                     * [length_of_reservoir/diameter_of_duck]
+    total_rubber_ducks = (math.floor(length_of_reservoir/diameter_of_duck)
+                        * math.floor(width_of_reservoir/diameter_of_duck))
+
+# output
+# print(“The number of rubber ducks required is” + total_rubber_ducks)
+    print("The number of rubber ducks required is " + str(total_rubber_ducks))
