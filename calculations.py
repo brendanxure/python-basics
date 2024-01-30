36b0081ced7b664632004abce26a0cb6def5af69
Brendan Obilo <brendanxure@outlook.com>
2024-01-30 20:01:46 +0100

    update

---
+++ calculations.py
@@ -0,0 +1,38 @@
+#######################################################
+#  Program title:         Calculations
+#  Program Author:        Brendan Obilo
+#  Date:                  24/01/2024
+#
+#  Description:           The use of arithmetic operations in Python.
+#########################################################
+
+# Initialization of variables
+slices_of_pizza = 50
+each_programmer_eats = 3
+addition = 0
+subraction = 0
+multiplication = 0
+division = 0.0
+pizza_remainder = 0
+
+# Addition
+addition = 120 + 43
+
+# Subraction
+subraction = 9382 - 450
+
+# Multiplication
+multiplication = 999 * 999
+
+# Division
+division = 120 / 40
+
+# Modulo
+pizza_remainder = slices_of_pizza % each_programmer_eats
+
+# output
+print(addition)
+print(subraction)
+print(multiplication)
+print(division)
+print(pizza_remainder)
