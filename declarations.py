12686717e756211cefd70c135243bf40f5da043f
Brendan Obilo <brendanxure@outlook.com>
2024-01-31 21:09:25 +0100

    update

---
+++ declarations.py
@@ -0,0 +1,17 @@
+#######################################################
+#  Program title:         Declarations
+#  Program Author:        Brendan Obilo
+#  Date:                  22/01/2024
+#
+#  Description:           Creating two variables, declaring them and declaring a constant according to PEP8 style guide. 
+#########################################################
+
+# Declaration of variables
+# The number of letters in my name
+letters_in_name = 7
+
+# My height in centimetres
+height = 175
+
+# The number of the days in a week
+DAYS_PER_WEEK = 7
