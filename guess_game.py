59a6290b30a9ce172b9cd715b21223aa186cc859
Brendan Obilo <brendanxure@outlook.com>
2024-03-17 23:32:13 +0100

    update

--- guess_game.py
+++ guess_game.py
@@ -35,6 +35,7 @@ while keep_guessing:
         counter += 1
         # user_input = int(input("Guess a number between " + str(min_num) + " and " + str(max_num) + " : "))
         user_input = int(input("Guess a number between " + str(min_num) + " and " + str(max_num) + " : "))
+        # if min_num < user_input or user_input < max_num:
         if min_num < user_input or user_input < max_num:
             if user_input == answer:
                 print("You have got the answer after " + str(counter) + " tries!")
