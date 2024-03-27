a37bc778b78553175f9642599acbb8a99c01bd87
Brendan Obilo <brendanxure@outlook.com>
2024-03-27 22:25:05 +0100

    update

--- guess_game.py
+++ guess_game.py
@@ -55,6 +55,7 @@ while keep_guessing:
                 max_num = user_input
                 # print("The number I have is smaller than " + str(user_input))
                 print("The number I have is smaller than " + str(user_input))
+        # else:
         else:
             print("Sorry, input must be between " + str(min_num) + " and " + str(max_num) + ", inclusively!")
     except ValueError: 
