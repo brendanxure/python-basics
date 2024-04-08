a24c12bc97509b152d6101dd2165f49435eaee1c
Brendan Obilo <brendanxure@outlook.com>
2024-04-08 20:17:24 +0100

    update

---
+++ tkinter_test.py
@@ -0,0 +1,14 @@
+#######################################################
+#  Program title:         tkinter test
+#  Program Author:        Brendan Obilo
+#  Date:                  26/03/2024
+#
+#  Description:           Created a user interface using tkinter
+#########################################################
+
+# from tkinter import *
+from tkinter import *
+# window = Tk()
+window = Tk()
+# window.mainloop()
+window.mainloop()
