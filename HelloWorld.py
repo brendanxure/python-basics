648f8ee6cd6c2f5a8def5527703ae1681dfb4675
Brendan Obilo <brendanxure@outlook.com>
2024-04-19 17:03:19 +0100

    update

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,3 +3,12 @@ print("Hello World!")
 print("Brendan Obilo")
 print("Computer Programming")
 print("2 years")
+
+score_list = [21,32,43,54,65,76,87]
+
+def rank_player(score_list = score_list):
+    remaining_slot = 10 - len(score_list)
+    for element in range(remaining_slot):
+        print('-')
+
+rank_player()
